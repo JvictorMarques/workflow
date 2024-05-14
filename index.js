072b660d5a6873ec4e5a31d7981c5bc9fa3e9291
JvictorMarques <jvictorsilvamarques358@gmail.com>
2024-05-14 14:27:26 -0300

    patch: Update index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core');
+
+try {
+    const input = core.getInput('my-input');
+    console.log(`My input is ${input}`);
+    core.setOutput("my-output", `Hello ${input}`);
+} catch (error) {core.setFailed(`Erro: ${error.message}`);}
+
